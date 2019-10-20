926d128993578ed03ebdb2583103e7fd64ee17b7
PavlovVasil <vasil@vasilpavlov.com>
2019-10-20 14:22:37 +0300

    Add starturls from old spider

--- scrapyAkordite/spiders/akordite_crawler.py
+++ scrapyAkordite/spiders/akordite_crawler.py
@@ -5,7 +5,7 @@ import scrapy
 class AkorditeCrawlerSpider(scrapy.Spider):
     name = 'akordite_crawler'
     allowed_domains = ['akordite.com']
-    start_urls = ['http://akordite.com/']
+    start_urls = ['http://www.akordite.com/index.php?option=com_chordbase&Itemid=26&initial=&limit=1527&limitstart=0']
 
     def parse(self, response):
         pass
